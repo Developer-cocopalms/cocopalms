1d72db848e9fda0c7719cc382c477d4f22e3409c
Developer-cocopalms <developer@cocopalms.io>
2025-08-02 18:11:59 +0300

    update

--- src/components/HeroVideoSlideshow.jsx
+++ src/components/HeroVideoSlideshow.jsx
@@ -287,7 +287,7 @@ const HeroVideoSlideshow = () => {
           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
             <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/>
           </svg>
-        </button>
+        </button> 
       </div>
 
       {/* Mobile Navigation Controls */}
